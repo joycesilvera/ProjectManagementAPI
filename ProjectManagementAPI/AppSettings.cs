07bb5b4ce1a1608a5c3d3cd45231cb16f92256a9
Joyce Silvera <60859729+joycesilvera@users.noreply.github.com>
2021-04-15 23:17:53 -0700

    added Login View Model and user authentication

---
+++ ProjectManagementAPI/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManagementAPI
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
+
+
---
+++ ProjectManagementAPI/ServiceContracts/IUsersService.cs
@@ -0,0 +1,14 @@
+﻿using ProjectManagementAPI.Identity;
+using ProjectManagementAPI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManagementAPI.ServiceContracts
+{
+    public interface IUsersService
+    {
+        Task<ApplicationUser> Authenticate(LoginViewModel loginViewModel);
+    }
+}
